2b19f112506b493da41c988a0fd5d21c309295b1
Chris <chris.goringe@gmail.com>
2025-02-19 16:24:44 +1100

    better file handling with txt added

--- blind_ab_scorer.py
+++ blind_ab_scorer.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 class MissingImageException(Exception): pass
 class AllDone(Exception): pass
+class ParameterException(Exception): pass
 
 IMAGE_EXT = [".jpg", ".jpeg", ".png"]
 
@@ -15,10 +16,26 @@ DEFAULT_PER_ROW_KP = [0,1,2,3,2,3,3,3,3,3]
 def is_image(filepath):
     return(os.path.isfile(filepath) and os.path.splitext(filepath)[1].lower() in IMAGE_EXT)
 
+def _move_file(basedir, subdir, filepath, verbose=False):
+    subdir = os.path.join(basedir, subdir)
+    if not os.path.exists(subdir): os.makedirs(subdir)
+    base, ext = os.path.splitext(os.path.basename(filepath))
+    newpath = os.path.join(subdir, base + ext)
+    while os.path.exists(newpath):
+        base = f"{base}_{random.randint(100000,999999)}"
+        newpath = os.path.join(subdir, base + ext)
+    shutil.move(filepath, newpath)
+    if verbose: print(f"Moved {filepath} to {newpath}")
+    txtfilepath = os.path.splitext(filepath)[0]+".txt"
+    if os.path.exists(txtfilepath):
+        newtxtpath = os.path.splitext(newpath)[0]+".txt"
+        shutil.move(txtfilepath, newtxtpath)
+        if verbose: print(f"Moved {txtfilepath} to {newtxtpath}")
+
 class ImageChooser:
-    def __init__(self, directory:str, match:Optional[str], rmatch:Optional[str], sub:list[str], 
-                 verbose:int, sort_mode:bool, recurse:bool, noshuffle:bool, allow_missing:bool, **kwargs):
-        self.directory = directory
+    def __init__(self, directory:list[str], match:Optional[str], rmatch:Optional[str]=None, sub:list[str]=None, 
+                 verbose:int=0, sort_mode:bool=False, recurse:bool=False, noshuffle:bool=False, allow_missing:bool=True, **kwargs):
+        self.directories = directory if isinstance(directory,list) else [directory,]
         self.verbose = verbose
 
         if sort_mode: matches = lambda a : True
@@ -27,10 +44,15 @@ class ImageChooser:
 
         candidate_imagepaths = []
         def scan_directory(d):
-            for f in os.listdir(d):
-                if matches(f) and is_image(os.path.join(d,f)):      candidate_imagepaths.append( os.path.join(d,f) )
-                if recurse    and os.path.isdir(os.path.join(d,f)): scan_directory(os.path.join(d,f))
-        scan_directory(self.directory)
+            try:
+                for f in os.listdir(d):
+                    if matches(f) and is_image(os.path.join(d,f)):      candidate_imagepaths.append( os.path.join(d,f) )
+                    if recurse    and os.path.isdir(os.path.join(d,f)): scan_directory(os.path.join(d,f))
+            except Exception as e:
+                print(f"Problem reading directory {d}")
+
+        for directory in self.directories:
+            scan_directory(directory)
 
         if not candidate_imagepaths:
             raise MissingImageException("No matching images found")
@@ -68,60 +90,79 @@ class ImageChooser:
 
         self.pointer = -1
 
+    def move_file(self, subdir, verbose=False):
+        assert(len(self.last_sent_images)==1)
+        assert(len(self.directories)==1)
+        _move_file(self.directories[0], subdir, self.last_sent_images[0], verbose)
+
     def guess_subs(self, one_match:str, match_term:str):
         before, after = one_match.split(match_term)
         subs = [match_term,]
-        for f in os.listdir(self.directory):
-            p = os.path.join(self.directory, f)
-            if p.startswith(before) and p.endswith(after):
-                mt = p[len(before):-len(after)]
-                if (mt!=match_term): subs += [mt,]
+        for directory in self.directories:
+            for f in os.listdir(directory):
+                p = os.path.join(directory, f)
+                if p.startswith(before) and p.endswith(after):
+                    mt = p[len(before):-len(after)]
+                    if (mt!=match_term): subs += [mt,]
         print(f"Guessing {subs}")
         return subs
 
-    def substituted(self, sub:str) -> Image.Image:
+    def substituted(self, sub:str, as_path=False) -> Image.Image:
         path = self.base_imagepath.replace(self.sub[0], sub)
+        if as_path: return path
         return Image.open(path) if os.path.exists(path) else Image.new('RGB', (32,32))
 
-    def next_image_set(self) -> tuple[Image.Image]:
+    def next_image_set(self, as_paths=False) -> tuple[Image.Image]:
         self.pointer += 1
         try:
             self.base_imagepath = self.base_imagepaths[self.pointer]
             if self.verbose: print(f"Loading base image {self.base_imagepath}")
-            images = list( self.substituted(sub) for sub in self.sub )
+            self.last_sent_images = list( self.substituted(sub, as_path=as_paths) for sub in self.sub )
             #if self.verbose: print(f"Loading subs {[self.base_imagepath.replace(self.sub[0], sub) for sub in self.sub]}")
         #except FileNotFoundError as e:
         #    raise MissingImageException(*e.args)
         except IndexError:
             raise AllDone()
         random.shuffle(self.order)
-        return tuple( images[self.order[i]] for i in range(self.batch_size) )
+        return tuple( self.last_sent_images[self.order[i]] for i in range(self.batch_size) )
 
-    def score(self, picked):
+    def score(self, picked, and_print=False):
         if not self.scores: self.scores = list(0 for _ in range(self.batch_size))
         set_number = self.order[picked]
         self.scores[set_number] += 1      
         if self.verbose>1: 
             picked_filename = os.path.split(self.base_imagepath)[1].replace(self.sub[0], self.sub[set_number])
             print(f"You chose image #{picked+1} which was {picked_filename}")
+        if and_print: self.print_scores()
 
     def scorelist(self, picked):
         if not self.scores: self.scores = list(list(0 for _ in range(self.batch_size)) for _ in range(self.batch_size))
         for i, pick in enumerate(picked):
             set_number = self.order[pick]
             self.scores[set_number][i] += 1
+
+    def print_scores(self):
+        if not self.scores: return ""
+        s = "\n".join(f"{label} : {self.scores[i]}" for i, label in enumerate(self.sub))
+        print(s)
+        return s
        
     @property
     def aspect_ratio(self):
-        with Image.open(os.path.join(self.directory, self.base_imagepaths[0])) as i:
-            return i.width / i.height 
+        try:
+            with Image.open(self.base_imagepaths[max(self.pointer,0)]) as i:
+                return i.width / i.height 
+        except:
+            return 1.0
+        
+
 
 class TheApp:
-    def __init__(self, ic:ImageChooser, height:int, perrow:int, keypad:bool, scorelist:bool, verbose:int, sort_mode:bool, directory:str, **kwargs):
+    def __init__(self, ic:ImageChooser, height:int, perrow:int, keypad:bool, scorelist:bool, verbose:int, sort_mode:bool, directory:list[str], move_chosen=None, **kwargs):
         self.app = customtkinter.CTk()
         self.header = 'ZM sort' if sort_mode else 'AB Compare'
         self.sort_mode = sort_mode
-        self.directory = directory
+        self.first_directory = directory[0]
 
         self.ic = ic
         self.height=height
@@ -144,6 +185,9 @@ class TheApp:
         
         for i, label in enumerate(self.image_labels): label.grid(row=(i//perrow), column=(i % perrow))
 
+        self.move_chosen = move_chosen
+        if self.move_chosen and (self.sort_mode):
+            raise ParameterException("move_chosen not compatible with sort_mode")
 
         self.app.bind("<KeyRelease>", self.keyup)
         self.pick_images()
@@ -152,8 +196,8 @@ class TheApp:
         
     def pick_images(self):
         try:
-            image_set = self.ic.next_image_set()
-            for i, im in enumerate(image_set):
+            self.image_set = self.ic.next_image_set()
+            for i, im in enumerate(self.image_set):
                 self.image_labels[i].configure(image = customtkinter.CTkImage(light_image=im, size=(int(self.height*im.width/im.height),self.height)))
             if self.scorelist: self.scores = []
             self.done += 1
@@ -163,6 +207,15 @@ class TheApp:
         self.set_title()
         self.last_picked_at = time.monotonic()
 
+    def move_basefile(self, subdir):
+        _move_file(self.first_directory, subdir, self.ic.base_imagepath, self.verbose)
+
+    def move_file(self, subdir, choice):
+        _move_file(self.first_directory, subdir, self.image_set[choice].filename, self.verbose)
+
+
+
+
     def keyup(self,k):
         if (time.monotonic() - self.last_picked_at)<0.2: return
         char = k.char or k.keysym
@@ -170,31 +223,32 @@ class TheApp:
         try:
             if self.sort_mode:
                 if char in 'zxcvbnm123456789':
-                    subdir = os.path.join(self.directory, char)
-                    if not os.path.exists(subdir): os.makedirs(subdir)
-                    base, ext = os.path.splitext(os.path.basename(self.ic.base_imagepath))
-                    newpath = os.path.join(subdir, base + ext)
-                    while os.path.exists(newpath):
-                        base = f"{base}.{random.randint(100000,999999)}"
-                        newpath = os.path.join(subdir, base + ext)
-                    shutil.move(self.ic.base_imagepath, newpath)
+                    self.move_basefile(char)
                 elif k.char==' ': 
                     pass
                 else:
                     return
             else:
-                if char==' ' and self.scorelist:
-                    self.ic.scorelist(self.scores)
+                if char==' ':
+                    if self.scorelist: 
+                        if self.move_chosen: 
+                            for c in self.scores: self.move_file(self.move_chosen, c)
+                        self.ic.scorelist(self.scores)
                 else:              
                     choice = int(self.keymap[int(char)])
                     if choice<self.ic.batch_size:
                         if (self.scorelist):
-                            self.scores.append(choice)
-                            if self.verbose: print(f"{self.scores}")
-                            if (len(self.scores)) != self.ic.batch_size: return
-                            self.ic.scorelist(self.scores)
+                            if not choice in self.scores:
+                                self.scores.append(choice)
+                                if self.verbose: print(f"{self.scores}")
+                                if (len(self.scores)) != self.ic.batch_size: return
+                                if self.move_chosen: 
+                                    for c in self.scores: self.move_file(self.move_chosen, c)
+                                self.ic.scorelist(self.scores)
+                            else: print("Repeat pick")
                         else:
                             self.ic.score(choice)
+                            if self.move_chosen: self.move_file(self.move_chosen, choice)
                     else: return
             self.pick_images()
         except AllDone:
@@ -211,7 +265,7 @@ class ArgumentException(Exception): pass
 
 def parse_arguments(override):
     parser = CommentArgumentParser("Blind compare image pairs. Normal usage python blind_ab_score.py @arguments.txt", fromfile_prefix_chars='@')
-    parser.add_argument('--directory', help="Directory images are in", required=True)
+    parser.add_argument('--directory', action="append", help="Directory images are in. Can be specified multiple times.", required=True)
     parser.add_argument('--recurse', action="store_true", help="include subdirectories")
     parser.add_argument('--rmatch', help="Optional regex to identify first image set. --match and --sub still used for replacements")
     parser.add_argument('--match', help="String to match to identify first image set")
@@ -223,6 +277,7 @@ def parse_arguments(override):
     parser.add_argument('--scorelist', action="store_true", help="Enter sequence of preferences ")
     parser.add_argument('--verbose', type=int, default=1, help="Verbosity 2 gives spoilers")
     parser.add_argument('--sort_mode', action="store_true", help="Ignore match and subs, show one image at a time, sort with 'z' and m'")
+    parser.add_argument('--move_chosen', type=str, help="Move first pick from each set to this subdirectory")
     parser.add_argument('--extensions', default=[], action="append", help="Extra extensions to count as images (.jpg, .jpeg and .png default)")
     parser.add_argument('--noshuffle', action='store_true', help="don't randomise the order of sets")
     parser.add_argument('--allow_missing', action='store_true', help="include sets with missing images")
@@ -256,11 +311,13 @@ def main():
     app.app.mainloop()
     if ic.scores is not None:
         print(f"{len(ic.base_imagepaths)} sets total")
-        for i, label in enumerate(ic.sub):
-            print(f"{label} : {ic.scores[i]}")
+        ic.print_scores()
+
 
 if __name__=="__main__":
     try: main()
     except ArgumentException as e:
+        print(e.args[0])
+    except MissingImageException as e:
         print(e.args[0])
     
---
+++ image_sorter.py
@@ -0,0 +1,76 @@
+import os, random, re, argparse, shutil
+
+def clean(dir:str) -> str:
+    drive, path = dir.split(":\\")
+    if not path:
+        path = drive
+        drive = None
+    rejoined = os.path.join(*path.split("\\"))
+    return drive + ":\\" + rejoined if drive else rejoined
+
+def regex_dir(dir:str, regex:re.Pattern, invert_regex:bool) -> bool: 
+    matches = any(regex.match(d) for d in dir.split(os.sep))
+    if invert_regex: matches = not matches
+    return matches
+
+def get_images(dir:str, ext:str=".png", recurse:bool=False, regex_str:str="", invert_regex=False):
+    regex = re.compile(regex_str)
+    if recurse:
+        all_files = [ os.path.join(root, file) for root, _, files in os.walk(dir) for file in files]
+    else:
+        all_files = [os.path.join(dir,f) for f in os.listdir(dir)]
+    return [f for f in all_files if os.path.splitext(f)[1].lower()==ext.lower() and regex_dir(dir=f, regex=regex, invert_regex=invert_regex)]
+
+def fix(source:str, prefix:str, target:str, extension=".png", recurse=False, start_idx=0, regex_str="", invert_regex=False):
+    idx = start_idx
+    prefix = prefix or f"{random.randint(100000,999999)}"
+
+    source = clean(source)
+    target = clean(target or source)
+    if not os.path.exists(target): os.makedirs(target, exist_ok=True)
+
+    original_paths = get_images(source, extension, recurse, regex_str, invert_regex)
+    newpath = lambda i:os.path.join(target, f"{prefix}_{i:0>5}{extension}")
+
+    for path in original_paths:
+        while os.path.exists(newpath(idx)): idx += 1
+        print(f"{path} -> {newpath(idx)}")
+        shutil.move(path, newpath(idx))
+        txtpath = os.path.splitext(path)[0] + ".txt"
+        if os.path.exists(txtpath): 
+            newtxtpath = os.path.split(newpath(idx))[0] + ".txt"
+            shutil.move(txtpath, newtxtpath)
+
+def distribute(source:str, recurse:bool, target:str, **kwargs):
+    fix(source, recurse=recurse, target=os.path.join(target,'bin'),        regex_str="^z$", **kwargs)
+    fix(source, recurse=recurse, target=os.path.join(target,'done'),       regex_str="^c$", **kwargs)
+    fix(source, recurse=recurse, target=os.path.join(target,'candidates'), regex_str="^x$", **kwargs)
+
+def remove_empties(source:str, **kwargs):
+    pass
+    
+def main():
+    a = argparse.ArgumentParser()
+
+    a.add_argument('--justfix', action="store_true", help="Just standardise names in directory")
+
+    a.add_argument('--source', default=r"A:\Images\tosort", help="source directory")
+    a.add_argument('--target', default=r"A:\Images", help="target for distribute")
+    a.add_argument('--prefix', default=None, help="prefix for sorted files. Default is a random 6 digit string")
+    a.add_argument('--norecurse', action='store_true', help="don't search source recursively")
+    a.add_argument('--extension', default=".png", help="extension to search for. Case insensitive")
+    a.add_argument('--keepempty', action='store_true', help="don't remove empty directories afterwards")
+
+    args = vars(a.parse_args())
+    args['recurse'] = not args.pop('norecurse')
+    
+    justfix = args.pop('justfix')
+    remove_empty = not args.pop('keepempty')
+
+    if justfix: fix(**args)
+    else:       
+        distribute(**args)
+        if remove_empty: remove_empties(**args)
+
+
+if __name__=='__main__': main()
